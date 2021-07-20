1c588e6a27c9582568a81967ed9e07dd414c0b4d
Théo Sigaud <sigaudtheo@gmail.com>
2021-07-20 12:43:32 +0200

    Fix verif

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -37,21 +37,14 @@ class BookController extends Controller
 
     function saveBook(Request $request)
     {
-
-        if (empty($request->name)||
-            empty($request->description)||
-            empty($request->category)){
-            return redirect('livres')->with('status', 'Veuillez remplir tous les champs');
-        }
-
+        
         $request->validate([
             'name' => ['required', 'max:255', 'min: 2'],
             'description' => ['required', 'min: 10', 'max: 255'],
         ],
-            [ 'name.required' => 'Le champs :attribute est requis.',
+            [ 'required' => 'Le champs :attribute est requis.',
                 'max' => 'Le champs :attribute ne doit pas comporter plus de :max',
                 'min' => 'Le champs :attribute ne doit pas comporter moins de :min'
-
             ]
         );
 
@@ -112,20 +105,14 @@ class BookController extends Controller
 
     function updateBook(Request $request){
 
-        if (empty($request->name)||
-            empty($request->description)||
-            empty($request->category)){
-            return redirect('livres')->with('status', 'Veuillez remplir tous les champs');
-        }
 
         $request->validate([
             'name' => ['required', 'max:255', 'min: 2'],
             'description' => ['required', 'min: 10', 'max: 255'],
         ],
-            [ 'name.required' => 'Le champs :attribute est requis.',
+            [ 'required' => 'Le champs :attribute est requis.',
                 'max' => 'Le champs :attribute ne doit pas comporter plus de :max',
                 'min' => 'Le champs :attribute ne doit pas comporter moins de :min'
-
             ]
         );
 
